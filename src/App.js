b8daa8840b828e2491d90718e118e0ac8a1f7b4b
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-05-12 16:06:32 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,10 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-    console.log(process.env)
+  constructor(props) {
+    super(props);
+    this.state = { data: {} };
   }
 
+  componentDidMount() {
+    console.log(process.env)
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showPosition);
+    }
+  }
+
+  showPosition = (position) => {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+
+    fetch('https://d7gsigzqe2.execute-api.eu-west-1.amazonaws.com/dev/getweather?lat=' +
+      position.coords.latitude + '&lon=' +
+      position.coords.longitude
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          data: data
+        });
+      })
+
+
+  }
 
   render() {
     return (
@@ -17,6 +43,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.data && this.state.data.currently && this.state.data.currently.summary}
         </p>
       </div>
     );
