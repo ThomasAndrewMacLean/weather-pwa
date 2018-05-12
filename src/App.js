a7576fd55ddc7fac4a085a2c526f26d7957c43df
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-05-12 16:20:11 +0200

    look

--- src/App.js
+++ src/App.js
@@ -38,12 +38,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          Whats the weather like...
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          {this.state.data && this.state.data.currently && this.state.data.currently.summary}
+
+          <code>
+
+            {this.state.data && this.state.data.currently && this.state.data.currently.summary}
+          </code>
         </p>
       </div>
     );
