4e8d55b09f4f477c0a23b9edbd5956af93c0399f
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-05-12 11:53:32 +0200

    add .env file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    console.log(process.env)
+  }
+
+
   render() {
     return (
       <div className="App">
